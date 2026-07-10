e5c24d89e9d4365788fbe609fc8c50cf24f53364
vagifrasulzade <vaqufresulzade@gmail.com>
2026-07-10 21:05:59 +0400

    Webpack

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
   module: {
     rules: [
       {
-      test: /\.(css)$/,
-      use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
